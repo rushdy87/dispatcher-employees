1fa5a17041dd649d4ab37388ee6c28e69efb5c7d
Mohammed Alrushdy <mohammed_alrushdy@yahoo.com>
2023-09-06 02:26:47 +0300

    Adding some styling to AddEmployeeForm

--- client/src/components/addEmployeeForm/AddEmployeeForm.jsx
+++ client/src/components/addEmployeeForm/AddEmployeeForm.jsx
@@ -56,121 +56,123 @@ const AddEmployeeForm = ({ setAddEmployees }) => {
   };
 
   return (
-    <div>
+    <div className='add-employee-container'>
       <form onSubmit={handleCreation}>
-        <div>
-          <label htmlFor='id'>رقم الحاسبة</label>
-          <input
-            type='number'
-            name='id'
-            id='id'
-            value={employee.id}
-            onChange={handleChange}
-          />
+        <div className='add-employee-form-container'>
+          <div className='add-employee-input-field'>
+            <label htmlFor='id'>رقم الحاسبة</label>
+            <input
+              type='number'
+              name='id'
+              id='id'
+              value={employee.id}
+              onChange={handleChange}
+            />
+          </div>
+          <div className='add-employee-input-field'>
+            <label htmlFor='name'>الاسم</label>
+            <input
+              type='text'
+              name='name'
+              id='name'
+              value={employee.name}
+              onChange={handleChange}
+            />
+          </div>
+          <div className='add-employee-input-field'>
+            <label htmlFor='status'>الحالة الوظيفية</label>
+            <select
+              name='status'
+              id='status'
+              value={employee.status}
+              onChange={handleChange}
+            >
+              <option value='ملاك' defaultValue>
+                ملاك
+              </option>
+              <option value='عقد'>عقد</option>
+            </select>
+          </div>
+          <div className='add-employee-input-field'>
+            <label htmlFor='workday'>نوع الدوام</label>
+            <select
+              name='workday'
+              id='workday'
+              value={employee.workday}
+              onChange={handleChange}
+            >
+              <option value='صباحي' defaultValue>
+                صباحي
+              </option>
+              <option value='مناوب'>مناوب</option>
+            </select>
+          </div>
+          <div className='add-employee-input-field'>
+            <label htmlFor='specialist_degree'>التخصص الدراسي</label>
+            <input
+              type='text'
+              name='specialist_degree'
+              id='specialist_degree'
+              value={employee.specialist_degree}
+              onChange={handleChange}
+            />
+          </div>
+          <div className='add-employee-input-field'>
+            <label htmlFor='joining_date'>تاريخ المباشرة</label>
+            <input
+              type='date'
+              name='joining_date'
+              id='joining_date'
+              value={employee.joining_date}
+              onChange={handleChange}
+            />
+          </div>
+          <div className='add-employee-input-field'>
+            <label htmlFor='phone_number'>رقم الهاتف</label>
+            <input
+              type='text'
+              name='phone_number'
+              id='phone_number'
+              value={employee.phone_number}
+              onChange={handleChange}
+            />
+          </div>
+          <div className='add-employee-input-field'>
+            <label htmlFor='birthdate'>تاريخ الميلاد</label>
+            <input
+              type='date'
+              name='birthdate'
+              id='birthdate'
+              value={employee.birthdate}
+              onChange={handleChange}
+            />
+          </div>
+          <div className='add-employee-input-field'>
+            <label htmlFor='address'>العنوان</label>
+            <input
+              type='text'
+              name='address'
+              id='address'
+              value={employee.address}
+              onChange={handleChange}
+            />
+          </div>
+          <div className='add-employee-input-field'>
+            <label htmlFor='gender'>النوع</label>
+            <select
+              name='gender'
+              id='gender'
+              value={employee.gender}
+              onChange={handleChange}
+            >
+              <option value='ذكر' defaultValue>
+                ذكر
+              </option>
+              <option value='أنثى'>أنثى</option>
+            </select>
+          </div>
+          <button>اضافة</button>
         </div>
-        <div>
-          <label htmlFor='name'>الاسم</label>
-          <input
-            type='text'
-            name='name'
-            id='name'
-            value={employee.name}
-            onChange={handleChange}
-          />
-        </div>
-        <div>
-          <label htmlFor='status'>الحالة الوظيفية</label>
-          <select
-            name='status'
-            id='status'
-            value={employee.status}
-            onChange={handleChange}
-          >
-            <option value='ملاك' defaultValue>
-              ملاك
-            </option>
-            <option value='عقد'>عقد</option>
-          </select>
-        </div>
-        <div>
-          <label htmlFor='workday'>نوع الدوام</label>
-          <select
-            name='workday'
-            id='workday'
-            value={employee.workday}
-            onChange={handleChange}
-          >
-            <option value='صباحي' defaultValue>
-              صباحي
-            </option>
-            <option value='مناوب'>مناوب</option>
-          </select>
-        </div>
-        <div>
-          <label htmlFor='specialist_degree'>التخصص الدراسي</label>
-          <input
-            type='text'
-            name='specialist_degree'
-            id='specialist_degree'
-            value={employee.specialist_degree}
-            onChange={handleChange}
-          />
-        </div>
-        <div>
-          <label htmlFor='joining_date'>تاريخ المباشرة</label>
-          <input
-            type='date'
-            name='joining_date'
-            id='joining_date'
-            value={employee.joining_date}
-            onChange={handleChange}
-          />
-        </div>
-        <div>
-          <label htmlFor='phone_number'>رقم الهاتف</label>
-          <input
-            type='text'
-            name='phone_number'
-            id='phone_number'
-            value={employee.phone_number}
-            onChange={handleChange}
-          />
-        </div>
-        <div>
-          <label htmlFor='birthdate'>تاريخ الميلاد</label>
-          <input
-            type='date'
-            name='birthdate'
-            id='birthdate'
-            value={employee.birthdate}
-            onChange={handleChange}
-          />
-        </div>
-        <div>
-          <label htmlFor='address'>العنوان</label>
-          <input
-            type='text'
-            name='address'
-            id='address'
-            value={employee.address}
-            onChange={handleChange}
-          />
-        </div>
-        <div>
-          <label htmlFor='gender'>النوع</label>
-          <select
-            name='gender'
-            id='gender'
-            value={employee.gender}
-            onChange={handleChange}
-          >
-            <option value='ذكر' defaultValue>
-              ذكر
-            </option>
-            <option value='أنثى'>أنثى</option>
-          </select>
-        </div>
-        <button>اضافة</button>
       </form>
     </div>
   );
