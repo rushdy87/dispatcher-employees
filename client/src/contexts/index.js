c3a446607265edf00e38fd03dab3d2b12bf552ca
Mohammed Alrushdy <mohammed_alrushdy@yahoo.com>
2023-09-12 03:04:05 +0300

    Remove the frontend

--- client/src/contexts/index.js
+++
@@ -1,9 +0,0 @@
-export {
-  EmployeesContext,
-  EmployeesContextProvider,
-} from './employees-context/EmployeesContext';
-
-export {
-  TableColumnsContext,
-  TableColumnsContextProvider,
-} from './table-columns-context/TableColumnsContext';
--- client/src/pages/index.js
+++
@@ -1,2 +0,0 @@
-export { default as Employees } from './employees/Employees';
-export { default as AddEmployees } from './addEmployees/AddEmployees';
